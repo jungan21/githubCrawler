6e00adce18dc98090dec0fb1bb2a530380e2f3fb
Jun Gan <jun.gan@scotiabank.com>
2019-09-22 20:56:34 -0400

    update to make it can crawl the a fold in Github repo

--- githubCrawler/items.py
+++ githubCrawler/items.py
@@ -11,4 +11,5 @@ import scrapy
 class GithubcrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    file_urls = scrapy.Field()
+    file = scrapy.Field()
--- githubCrawler/spiders/example.py
+++ githubCrawler/spiders/example.py
@@ -1,33 +1,52 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
-from bs4 import BeautifulSoup
-from scrapy import Request
-
-import json
-import re
+from ..items import GithubcrawlerItem
 
 
-class ExampleSpider(scrapy.Spider):
+class AppSpider(scrapy.Spider):
     name = 'example'
     allowed_domains = ['github.com']
-    start_urls = ["https://github.com/pnorman/josm-presets/blob/master/shops.xml"]
+    content_domains = 'https://github.com/'
+    start_urls = []
+
+
+
+    def __init__(self, urls=None, *args, **kwargs):
+        super(AppSpider, self).__init__(*args, **kwargs)
+        self.start_urls = urls.split(',')
 
     def parse(self, response):
-        soup = BeautifulSoup(response.text, features='lxml')
-        web_text = soup.get_text()
+        raw_url = response.css('a#raw-url').xpath('@href').extract_first()
+        if raw_url:
+                href = self.content_domains+raw_url
+                print("scrapy from href --> ", href)
+                yield scrapy.Request(href, callback=self.parse_link)
+        else:
+            for link in response.selector.xpath('//a[@class="js-navigation-open"]/@href').extract()[1:]:
+                href = self.content_domains+link
+                yield scrapy.Request(href, callback=self.parse)
 
-        sensitive_words = ["Account", "Password", "scotiabank", "wechat"]
-        output_found = "FOUND Sensitive word: {0}"
-        output_not_found= "Can't find Sensitive word: {0}"
+    def parse_link(self, response):
+        responseStr = str(response).strip()
+        # print('Jun response.text --> ', response.text)
+        url = responseStr.strip()[5:len(responseStr) - 1]
 
 
+        sensitive_words = ["Account", "Password", "scotiabank", "colorPrimary"]
+        output_found = "FOUND SENSITIVE Information: {0} in file {1}"
+        output_not_found = "Can't find Sensitive word: {0}"
+        output_start_check = "<========================= Start Checking Sensitive Information for file: {0} =========================>"
+        output_end_check = "<========================= End Checking Sensitive Information for file: {0} =========================>"
+
+        print(output_start_check.format(url))
         for word in sensitive_words:
-            if ( web_text.find (word) == -1):
-                print(output_not_found.format(word))
-            else:
-                print(output_found.format(word))
+            if (response.text.find(word) != -1):
+                print(output_found.format(word, responseStr))
+        print(output_end_check.format(url))
 
+        item = GithubcrawlerItem()
+        item['file_urls'] = [url]
+        return item
 
 
 
